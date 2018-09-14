856baee79f15e7e96559a1cfcb619ef4bdd25ef9
Fafianie <Stefan.Fafianie@ordina.nl>
2018-09-14 13:09:27 +0200

    messing around with cmake asm

--- project/src/galois/test/pcmul_test.cpp
+++ project/src/galois/test/pcmul_test.cpp
@@ -1,9 +1,16 @@
 #include "stdafx.h"
+#include "galois.h"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	Galois galois;
+	galois.set_mode_pcmul();
+	uint64_t a = galois.uniform_random_element();
+	uint64_t b = galois.uniform_random_element();
+	galois.multiply(a, b);
+
 	cout << "Test passed" << endl;
 	return 0;
 }
