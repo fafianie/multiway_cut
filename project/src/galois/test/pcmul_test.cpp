0e8e23ad4ce0d2ea1413a55bba419dcfa67d0796
fafianie <s.fafiani@gmail.com>
2018-09-17 22:13:05 +0200

    fixed galois asm for g++

--- project/src/galois/test/pcmul_test.cpp
+++ project/src/galois/test/pcmul_test.cpp
@@ -1,7 +1,25 @@
 #include "stdafx.h"
 #include "galois.h"
+#include "powers.cpp"
 
 using namespace std;
+
+/*extern "C" {
+	uint64_t pclmul(uint64_t a, uint64_t b);
+	uint64_t fetch1(uint64_t mask);
+	uint64_t fetch2();
+}
+
+uint64_t clmul_multiply(uint64_t arg1, uint64_t arg2)
+{
+	uint64_t mask = pwm1[32];
+	uint64_t x1x0 = pclmul(arg1, arg2);
+	uint64_t x2 = fetch1(mask);
+	uint64_t x3 = fetch2();
+	uint64_t x3d = (x3 << 32) ^ (x3 >> 31) ^ (x3 >> 29) ^ (x3 >> 28) ^ x2;
+	uint64_t h1h0 = (x3d << 1) ^ (x3d << 3) ^ (x3d << 4) ^ x3d;
+	return h1h0 ^ x1x0;
+}*/
 
 int main(int argc, char* argv[])
 {
@@ -10,6 +28,7 @@ int main(int argc, char* argv[])
 	uint64_t a = galois.uniform_random_element();
 	uint64_t b = galois.uniform_random_element();
 	galois.multiply(a, b);
+	//clmul_multiply(1, 2);
 
 	cout << "Test passed" << endl;
 	return 0;
