8cb7aa47a07d4d9015c01b187f4dfd741beb7b71
fafianie <s.fafiani@gmail.com>
2018-09-24 23:45:26 +0200

    temp fix for pcmul

--- project/src/galois/src/carryless_multiplier_galois.cpp
+++ project/src/galois/src/carryless_multiplier_galois.cpp
@@ -19,16 +19,18 @@ uint64_t CarrylessMultiplierGalois::divide(uint64_t leftOperand, uint64_t rightO
 }
 
 uint64_t CarrylessMultiplierGalois::multiply(uint64_t leftOperand, uint64_t rightOperand) {
-	uint64_t mask = pwm1[32];
+	/*uint64_t mask = pwm1[32];
 	uint64_t x1x0 = pclmul(leftOperand, rightOperand);
 	uint64_t x2 = fetch1(mask);
 	uint64_t x3 = fetch2();
 	uint64_t x3d = (x3 << 32) ^ (x3 >> 31) ^ (x3 >> 29) ^ (x3 >> 28) ^ x2;
 	uint64_t h1h0 = (x3d << 1) ^ (x3d << 3) ^ (x3d << 4) ^ x3d;
-	return h1h0 ^ x1x0;
-	/*uint64_t mask = pwm1[32];
+	return h1h0 ^ x1x0;*/
+	uint64_t mask = pwm1[32];
 	uint64_t x1x0, x2, x3;
-  
+	
+	//leftOperand = pwm1[32];
+	//rightOperand = pwm1[32];
 	/* __asm__ (
 		//carry-less multiplication
 		"movd %3, %%xmm0;" 
@@ -60,7 +62,34 @@ uint64_t CarrylessMultiplierGalois::multiply(uint64_t leftOperand, uint64_t righ
 
 		: "=r" (x1x0), "=r" (x2), "=r" (x3)
 		: "r" (leftOperand), "r" (rightOperand), "r" (mask)		
+	);*/
+	
+	std::cout << " left: " << toString(leftOperand) << std::endl 
+			<< "right: " << toString(rightOperand) << std::endl;
+	
+	
+	uint64_t x2x3;
+	
+	__asm__ (
+		"movd %2, %%xmm0;" 
+	 	"movd %3, %%xmm1;"
+		"pclmulqdq $0, %%xmm0, %%xmm1;" 
+		"movd %%xmm1, %0;"
+		
+		"psrldq $8, %%xmm1;"
+		"movd %%xmm1, %1;"
+	
+		: "=r" (x1x0), "=r" (x2x3)
+		: "r" (leftOperand), "r" (rightOperand)
 	);
+	
+	x2 = x2x3 & mask;
+	x3 = x2x3 >> 32;
+	
+	std::cout << " x1x0: " << toString(x1x0) << std::endl 
+			  << " x2x3: " << toString(x2x3) << std::endl 
+			  << "   x2: " << toString(x2) << std::endl 
+			  << "   x3: " << toString(x3) << std::endl;
 	
 	/*__asm__ ( 	//carry-less multiplication
 		"movd %3, %%xmm0;" 
@@ -86,7 +115,7 @@ uint64_t CarrylessMultiplierGalois::multiply(uint64_t leftOperand, uint64_t righ
 	//uint64_t x2 = x2unmasked ^ mask;
 
 	//modulo primitive polynomial
-	/*uint64_t x3d = (x3 << 32) ^ (x3 >> 31) ^ (x3 >> 29) ^ (x3 >> 28) ^ x2;
+	uint64_t x3d = (x3 << 32) ^ (x3 >> 31) ^ (x3 >> 29) ^ (x3 >> 28) ^ x2;
 	uint64_t h1h0 = (x3d << 1) ^ (x3d << 3) ^ (x3d << 4) ^ x3d;
-	return h1h0 ^ x1x0;*/
+	return h1h0 ^ x1x0;
 }
