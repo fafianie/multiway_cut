4d7089016d874dcff60450b66da3d2c44b30edcf
fafianie <s.fafiani@gmail.com>
2019-01-05 10:06:59 +0100

    t

--- project/src/solvers/test/dummy_test.cpp
+++ project/src/solvers/test/dummy_test.cpp
@@ -7,35 +7,38 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	
-	Graph* graph = new PlantedInstance(100, 7, 5, 300 ,10, 20, 4);
+	for (int repetition = 0; repetition < 1000; repetition++) {
+		Graph* graph = new PlantedInstance(100, 7, 5, 300 ,10, 20, 4);
 	
-	int** adjacency = new int*[100];
-	for (int i = 0; i < graph -> getVertices(); i++) {
-		adjacency[i] = new int[100];
-		for (int j = 0; j < graph -> getVertices(); j++) {
-			if (graph -> isOutNeighbor(i, j)){
-				adjacency[i][j] = 1;
-			} else {
-				adjacency[i][j] = 0;
+		int** adjacency = new int*[100];
+		for (int i = 0; i < graph -> getVertices(); i++) {
+			adjacency[i] = new int[100];
+			for (int j = 0; j < graph -> getVertices(); j++) {
+				if (graph -> isOutNeighbor(i, j)){
+					adjacency[i][j] = 1;
+				} else {
+					adjacency[i][j] = 0;
+				}
 			}
 		}
-	}
-	vector<int> terminals;
-	for (auto terminal : graph -> getTerminals()) {
-		terminals.push_back(terminal);
-	}
+		vector<int> terminals;
+		for (auto terminal : graph -> getTerminals()) {
+			terminals.push_back(terminal);
+		}
 	
 	
-	MWCSolver* solver = new MWCSolver(graph -> getVertices(), 
-								 graph -> getNumberOfTerminals(),
-								 terminals,
-								 adjacency);
-	int solution = solver -> solve();
-	if (solution > 4) {
-		return 1;
+		MWCSolver* solver = new MWCSolver(graph -> getVertices(), 
+										  graph -> getNumberOfTerminals(),
+										  terminals,
+										  adjacency);
+		int solution = solver -> solve();
+		if (solution > 4) {
+			return 1;
+		}
+	
+		delete graph;
 	}
 	
-	delete graph;
 	
 	return 0;
 }
