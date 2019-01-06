3f8f452d238325927412691987c15e2bc0cefa47
fafianie <s.fafiani@gmail.com>
2019-01-06 17:37:15 +0100

    t

--- project/src/solvers/test/correctness_test.cpp
+++ project/src/solvers/test/correctness_test.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
 	
 	vector<pair<string, int>> instances;
 	instances.push_back({"3__sparse_planar600_3.dgf", 3});
-	//instances.push_back({"10__sparse_codeminer_4.dgf", 10});
+	instances.push_back({"10__sparse_codeminer_4.dgf", 10});
 	//instances.push_back({"11__sparse_fvs056_4.dgf", 11});
 	//instances.push_back({"16__sparse_planar400_0.dgf", 16});
 	//instances.push_back({"19__sparse_clh-10-1_3.dgf", 19});
