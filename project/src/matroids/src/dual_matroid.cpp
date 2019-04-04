99b2fd683f82b7cd82e20724bce1cbc0bed4ff33
fafianie <s.fafiani@gmail.com>
2019-04-04 23:08:45 +0200

    t

--- project/src/matroids/src/dual_matroid.cpp
+++ project/src/matroids/src/dual_matroid.cpp
@@ -6,6 +6,32 @@
 
 using namespace std;
 
-Matroid DualMatroid::generate(Matroid& matroid, Galois* galois) {
+Matroid DualMatroid::generate(Matroid& inputMatroid, Galois* galois) {
+	//copy the matroid first
+	int numberOfElements = inputMatroid.getNumberOfElements()
+	int rank = inputMatroid.getRank();
+	Matroid outputMatroid(numberOfElements, rank)
+	for (int column = 0; column < elements, column++) {
+		for (int row = 0; row < rank; rank++) {	
+			uint64_t value = inputMatroid.getField(column, row);
+			outputMatroid.setField(column, row, value);
+		}
+	} //aliases?
+	
+	
+	
+	
 	return matroid;
+}
+
+void DualMatroid::swipeDown() {
+	
+}
+
+void DualMatroid::swipeUp() {
+	
+}
+
+void DualMatroid::normalize() {
+	
 }
