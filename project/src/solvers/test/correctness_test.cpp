1a5b26ea9d9e19936a575986dfb931d6c67afa32
fafianie <s.fafiani@gmail.com>
2019-09-02 21:24:36 +0200

    t

--- project/src/solvers/test/correctness_test.cpp
+++ project/src/solvers/test/correctness_test.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	
+	cout << "start test" << endl;
 	vector<pair<string, int>> instances;
 	//instances.push_back({"3__sparse_planar600_3.dgf", 3});
 	instances.push_back({"10__sparse_codeminer_4.dgf", 10});
@@ -17,14 +18,19 @@ int main(int argc, char* argv[]) {
 	//instances.push_back({"19__sparse_hex_3.dgf", 19});
 	//instances.push_back({"23__sparse_fvs100_4.dgf", 23});
 	
+	
+	
 	MWCSolver* solver;
 	for (pair<string, int> instance : instances) {
+		cout << "start read" << endl;
 		Graph graph = DGFReader::read("../../resources/test/correctness/", instance.first);	
-		
+		cout << "graph is loaded" << endl;
 		
 	
 		MWCSolver* solver = new MWCSolver();
+		cout << "solver created" << endl;
 		int solution = solver -> solve(graph);
+		cout << "solution retrieved" << endl;
 		if (!(solution == instance.second)) {
 			return 1;
 		}
